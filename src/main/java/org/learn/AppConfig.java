13d94a3e383199baf23b4be140dc134bf6df6463
Ravi-Kiran-Bhusetty <ravikiranb.2205@gmail.com>
2024-03-18 18:41:00 +0530

    Prototype Scope

--- src/main/java/org/learn/AppConfig.java
+++ src/main/java/org/learn/AppConfig.java
@@ -2,6 +2,7 @@ package org.learn;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 // Config class. Defined by below annotation
 @Configuration
@@ -9,6 +10,7 @@ public class AppConfig {
 
     // We return the object. But lifecycle is maintained by Spring
     @Bean
+    @Scope("prototype")
     public Alien alien() {
         return new Alien();
     }
